308a5096ab2ab74de5929647a73b337865995182
Jesica56 <martinezjesicanoelia56@gmail.com>
2023-11-27 09:36:23 -0300

    cambios en la img del carrusel

--- src/components/Carousel/CarouselInicio.tsx
+++ src/components/Carousel/CarouselInicio.tsx
@@ -8,7 +8,7 @@ const CarouselInicio = () => {
         <img 
         className='d block w-100'
         style={{maxHeight:"500px", objectFit:'cover'}}
-        src="https://www.freecodecamp.org/news/content/images/2022/12/main-image.png" alt="" />
+        src="https://factorialhr.es/wp-content/uploads/2021/04/07154848/gestion-de-proyectos-min.jpg" alt="" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -18,7 +18,7 @@ const CarouselInicio = () => {
         <img 
         className='d block w-100'
         style={{maxHeight:"500px", objectFit:'cover'}}
-        src="https://www.itprotoday.com/sites/itprotoday.com/files/styles/article_featured_retina/public/programming.jpg?itok=6bRkryO5" alt=""/>
+        src="https://www.yunbitsoftware.com/blog/wp-content/uploads/2021/02/21430-scaled-e1614339566329.jpg" alt=""/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -28,7 +28,7 @@ const CarouselInicio = () => {
         <img 
         className='d block w-100'
         style={{maxHeight:"500px", objectFit:'cover'}}
-        src="https://i.ytimg.com/vi/kX0tq3qsY_U/maxresdefault.jpg" alt=""/>
+        src="https://www.esan.edu.pe/images/blog/2021/12/13/1500x844-viabilidad-13-12-2021.jpg" alt=""/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
